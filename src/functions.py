b1bbeebfe9b168e55743decba6a525c116e008db
graceclaudia19 <graceclaudia78@gmail.com>
2022-04-03 04:59:29 +0700

    add comments and asciis

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,11 @@
-from turtle import position
-
-from numpy import mat
 import pq
 
-
 def kurang(m, el, iNow, jNow):
+    # Penjelasan:
+    # fungsi ini berguna untuk menghitung banyak tile j
+    # dimana j < i dan POSISI(j) > POSISI(i)
+    # fungsi ini akan membantu perhitungan apakah puzzle dapat diselesaikan atau tidak
+    # fungsi ini akan mengembalikan nilai kurang(i)
     less = 0
     for i in range (iNow,4):
         if i==iNow:
@@ -19,7 +20,39 @@ def kurang(m, el, iNow, jNow):
                     less+=1
     return less
 
+def reachableGoal(m):
+    # Penjelasan:
+    # fungsi ini akan menentukan apakah persoalan dapat diselesaikan atau tidak
+    # x merupakan nilai 1/0, sel yang diarsir akan bernilai 1, tidak diarsir bernilai 0
+    # reachableGoal dilakukan dengan menghitung sigma Kurang(i) + X
+    # jika sigma Kurang(i) + x ganjil maka tidak dapat dicapai, akan mereturn FALSE
+    # sebaliknya, apabila hasil genap maka akan mereturn TRUE
+    totLess = 0
+    # checking x
+    kurangTable = [0 for i in range (17)]
+    for i in range(4):
+        for j in range(4):
+            if (m[i][j]>16):
+                return False
+            if (m[i][j]==16):
+                if ((i+j)%2==0):
+                    x = 0
+                else:
+                    x = 1
+            less = kurang(m, m[i][j], i, j)
+            totLess += less
+            kurangTable[m[i][j]] = less
+    displayKurangTable(kurangTable, x, totLess)
+    if (x+totLess)%2==0:
+        return True
+    else:
+        return False
+
 def cost(m):
+    # Penjelasan:
+    # fungsi ini berguna untuk menghitung cost dengan cara banyaknya ubin tidak kosong yang 
+    # tidak terdapat pada susunan akhir
+    # fungsi ini mengembalikan cost suatu node
     costs = 0
     ans = [[1,2,3,4],
            [5,6,7,8],
@@ -31,14 +64,25 @@ def cost(m):
                 if m[i][j]!=ans[i][j]:
                     costs+=1
     return costs
-                
-def blankPosition(m):
-    for i in range (4):
-        for j in range (4):
-            if m[i][j] == 16:
-                return [i,j]
+
+def copyMatrix(m):
+    # Penjelasan:
+    # fungsi ini digunakan untuk mengcopy suatu matrix ke matrix lain
+    # guna dari pengc-copy-an matrix ini adalah agar tidak merusak sebuah matrix saat dilakukan deep copy
+    # fungsi ini mengembalikan matrix yang telah tercopy
+    newM = []
+    for i in range(4):
+        newM.append([])
+        for j in range(4):
+            newM[i].append(m[i][j])
+    return newM
                 
 def moveUp(m):
+    # Penjelasan:
+    # fungsi ini ditujukan untuk mengubah posisi ubin kosong ke atas
+    # fungsi ini akan mendeep copy matrix sesuai dengan pergeseran yang dilakukan
+    # pergeseran terlebih dahulu di cek apakah memungkinkan (masih dalam ukuran 4x4)
+    # jika tidak memenuhi maka tidak akan melakukan perubahan pada matrix
     found = False
     for i in range (4):
         for j in range (4):
@@ -47,6 +91,11 @@ def moveUp(m):
                 found = True
 
 def moveDown(m):
+    # Penjelasan:
+    # fungsi ini ditujukan untuk mengubah posisi ubin kosong ke bawah
+    # fungsi ini akan mendeep copy matrix sesuai dengan pergeseran yang dilakukan
+    # pergeseran terlebih dahulu di cek apakah memungkinkan (masih dalam ukuran 4x4)
+    # jika tidak memenuhi maka tidak akan melakukan perubahan pada matrix
     found = False
     for i in range (4):
         for j in range (4):
@@ -56,6 +105,11 @@ def moveDown(m):
     
 
 def moveRight(m):
+    # Penjelasan:
+    # fungsi ini ditujukan untuk mengubah posisi ubin kosong ke kanan
+    # fungsi ini akan mendeep copy matrix sesuai dengan pergeseran yang dilakukan
+    # pergeseran terlebih dahulu di cek apakah memungkinkan (masih dalam ukuran 4x4)
+    # jika tidak memenuhi maka tidak akan melakukan perubahan pada matrix
     found = False
     for i in range (4):
         for j in range (4):
@@ -64,6 +118,11 @@ def moveRight(m):
                 found = True
 
 def moveLeft(m):
+    # Penjelasan:
+    # fungsi ini ditujukan untuk mengubah posisi ubin kosong ke kiei
+    # fungsi ini akan mendeep copy matrix sesuai dengan pergeseran yang dilakukan
+    # pergeseran terlebih dahulu di cek apakah memungkinkan (masih dalam ukuran 4x4)
+    # jika tidak memenuhi maka tidak akan melakukan perubahan pada matrix
     found = False
     for i in range (4):
         for j in range (4):
@@ -72,6 +131,8 @@ def moveLeft(m):
                 found = True
 
 def displayMatrix(m):
+    # Penjelasan:
+    # fungsi ini melakukan pencetakan matrix ke layar
     print("╔════╤════╤════╤════╗")
     for i in range(4):
         print("║", end=" ")
@@ -86,106 +147,42 @@ def displayMatrix(m):
             print("\n╟────┼────┼────┼────╢")
     print("\n╚════╧════╧════╧════╝")    
 
-def copyMatrix(m):
-    newM = []
-    for i in range(4):
-        newM.append([])
-        for j in range(4):
-            newM[i].append(m[i][j])
-    return newM
-
 def isSame(m1, m2):
+    # Penjelasan:
+    # fungsi ini membandingkan dua buah matrix yang ada di parameter
+    # jika sama mereturn TRUE, jika berbeda mereturn FALSE
     for i in range(4):
         for j in range(4):
             if m1[i][j] != m2[i][j]:
                 return False
     return True
 
-def IsNotInMatrixBefore(m, matrixesBefore):
-    for matrix in matrixesBefore:
-        if isSame(m, matrix):
-            return False 
-    return True
-
 def isNotInListOfNodes(nodeToCheck, listOfNodes):
+    # Penjelasan:
+    # fungsi ini mengeccek apakah sebuah node tidak berada di sebuah list of nodes
+    # jika ada akan mereturn FALSE, jika tidak ada akan TRUE 
     for node in listOfNodes:
         if isSame(node.mat, nodeToCheck.mat):
             return False
     return True
 
-def generateChildNode(parent, visitedNodes):
-    generatedNodes = []
-
-    parMatrix = copyMatrix(parent.mat)    
-
-    # DOWN
-    mDown = copyMatrix(parMatrix)
-    moveDown(mDown)
-    nodeDown = pq.node(parMatrix,mDown, cost(mDown), parent.level+1, "down")
-    if isNotInListOfNodes(nodeDown, visitedNodes):
-        generatedNodes.append(nodeDown)
-    
-    # UP
-    mUp = copyMatrix(parMatrix)
-    moveUp(mUp)
-    nodeUp = pq.node(parMatrix,mUp, cost(mUp), parent.level +1, "up")
-    if isNotInListOfNodes(nodeUp, visitedNodes):
-        generatedNodes.append(nodeUp)
-    
-    # RIGHT
-    mRight = copyMatrix(parMatrix)
-    moveRight(mRight)
-    nodeRight = pq.node(parMatrix,mRight, cost(mRight), parent.level+1, "right")
-    if isNotInListOfNodes(nodeRight, visitedNodes):
-        generatedNodes.append(nodeRight)
-    
-    # LEFT
-    mLeft = copyMatrix(parMatrix)
-    moveLeft(mLeft)
-    nodeLeft = pq.node(parMatrix,mLeft, cost(mLeft), parent.level+1, "left")
-    if isNotInListOfNodes(nodeLeft, visitedNodes):
-        generatedNodes.append(nodeLeft)
-
-    return generatedNodes
-
 def displayInfo(step, node):
-    print("-- S T E P ",step,"--")
-    print("Cost  :", node.cost)
-    print("Level :", node.level)
-    print("Move  :", node.move)
+    # Penjelasan:
+    # fungsi ini digunakan untuk mencetak info dari setiap pengiterasian yang digunakan ke layar
+    if step != 0:
+        print("-- S T E P ",step,"--")
+        print("Cost  :", node.cost)
+        print("Level :", node.level)
+        print("Move  :", node.move)
+    else:
+        print("INITIAL PUZZLE STATE")
     displayMatrix(node.mat)
-    if node.cost!=0:
-        print("\n")
-    
-def BnB(m):
-    PQ = pq.priorityQueue()
-
-    parent = pq.node(None,m,cost(m), 0, "initial position")
-    visitedNodes = [parent]
-    PQ.push(parent)
-
-    step = 0
-    while not(PQ.isEmpty()):
-        # pop infront of prioqueue
-        parent = PQ.pop()        
-        # visit parent
-        visitedNodes.append(parent)
-
-        displayInfo(step, parent)
-
-        # check if current node is goal
-        if parent.cost == 0:
-            print("\n\( ﾟヮﾟ)/ -- YOUR PUZZLE IS SOLVED! -- ٩(◍╹∀╹◍)۶")
-            break
-        else:
-            # generate child node
-            childNodes = generateChildNode(parent, visitedNodes)
-            # push child node to prioqueue
-            for child in childNodes:
-                PQ.push(child)
-        step += 1
+    print("\n")
 
 def displayKurangTable(kurangTable,x, totless):
+    # Penjelasan:
+    # fungsi ini digunakan untuk mencetak i dan nilai masing-masing yaitu kurang(i) secara berurutan ke layar
+    # fungsi ini juga mencetak sigma dari kurang(i) dan x serta pertambahan keduanya
     print(" -------------------")
     print("|  i   |  Kurang(i) |")
     print(" -------------------")
@@ -207,36 +204,109 @@ def displayKurangTable(kurangTable,x, totless):
     print("\nΣ Kurang(i)     =",str(totless))
     print("Nilai x         =",str(x))
     print("Σ Kurang(i) + x =", str(totless + x))
-        
+    print("\n")
 
+def generateChildNode(parent, visitedNodes):
+    # Penjelasan:
+    # fungsi untuk mengenerate ruang pohon status sesuai parent node(cost terkecil) 
+    # fungsi ini akan mngembalikan ARRAY OF GENERATED NODES 
 
-# check whether it can be solved or not,input matrix, return bool
-def reachableGoal(m):
-    print("""
- ██╗███████╗    ██████╗ ██╗   ██╗███████╗███████╗██╗     ███████╗    ███████╗ ██████╗ ██╗    ██╗   ██╗███████╗██████╗ 
-███║██╔════╝    ██╔══██╗██║   ██║╚══███╔╝╚══███╔╝██║     ██╔════╝    ██╔════╝██╔═══██╗██║    ██║   ██║██╔════╝██╔══██╗
-╚██║███████╗    ██████╔╝██║   ██║  ███╔╝   ███╔╝ ██║     █████╗      ███████╗██║   ██║██║    ██║   ██║█████╗  ██████╔╝
- ██║╚════██║    ██╔═══╝ ██║   ██║ ███╔╝   ███╔╝  ██║     ██╔══╝      ╚════██║██║   ██║██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗
- ██║███████║    ██║     ╚██████╔╝███████╗███████╗███████╗███████╗    ███████║╚██████╔╝███████╗╚████╔╝ ███████╗██║  ██║
- ╚═╝╚══════╝    ╚═╝      ╚═════╝ ╚══════╝╚══════╝╚══════╝╚══════╝    ╚══════╝ ╚═════╝ ╚══════╝ ╚═══╝  ╚══════╝╚═╝  ╚═╝                                                                                                     
-    """)
-    totLess = 0
-    # checking x
-    kurangTable = [0 for i in range (17)]
-    for i in range(4):
-        for j in range(4):
-            if (m[i][j]>16):
-                return False
-            if (m[i][j]==16):
-                if ((i+j)%2==0):
-                    x = 0
-                else:
-                    x = 1
-            less = kurang(m, m[i][j], i, j)
-            totLess += less
-            kurangTable[m[i][j]] = less
-    displayKurangTable(kurangTable, x, totLess)
-    if (x+totLess)%2==0:
-        return True
-    else:
-        return False
+    # menginisialisasi tempat penyimpanan node yaitu pada sebuah array
+    generatedNodes = []
+
+    # mengcopy matrix agar menjaga tidak terubah saat deep copy
+    parMatrix = copyMatrix(parent.mat)    
+
+    # DOWN
+    # mengcopy matrix parent yang nantinya akan dilakukan pergeseran ke bawah
+    mDown = copyMatrix(parMatrix)
+    # melakukan pergeseran ke bawah
+    moveDown(mDown)
+    # membuat node baru sesuai atribut pada kelas node
+    nodeDown = pq.node(parMatrix,mDown, cost(mDown), parent.level+1, "down")
+    # jika node yang baru dibuat tidak ada di list of visitedNodes, maka node akan ditambahkan ke list of nodes
+    if isNotInListOfNodes(nodeDown, visitedNodes):
+        generatedNodes.append(nodeDown)
+    
+    # UP
+    # mengcopy matrix parent yang nantinya akan dilakukan pergeseran ke atas
+    mUp = copyMatrix(parMatrix)
+    # melakukan pergeseran ke atas
+    moveUp(mUp)
+    # membuat node baru sesuai atribut pada kelas node
+    nodeUp = pq.node(parMatrix,mUp, cost(mUp), parent.level +1, "up")
+    # jika node yang baru dibuat tidak ada di list of visitedNodes, maka node akan ditambahkan ke list of nodes
+    if isNotInListOfNodes(nodeUp, visitedNodes):
+        generatedNodes.append(nodeUp)
+    
+    # RIGHT
+    # mengcopy matrix parent yang nantinya akan dilakukan pergeseran ke kanan
+    mRight = copyMatrix(parMatrix)
+    # melakukan pergeseran ke kanan
+    moveRight(mRight)
+    # membuat node baru sesuai atribut pada kelas node
+    nodeRight = pq.node(parMatrix,mRight, cost(mRight), parent.level+1, "right")
+    # jika node yang baru dibuat tidak ada di list of visitedNodes, maka node akan ditambahkan ke list of nodes
+    if isNotInListOfNodes(nodeRight, visitedNodes):
+        generatedNodes.append(nodeRight)
+    
+    # LEFT
+    # mengcopy matrix parent yang nantinya akan dilakukan pergeseran ke kiri
+    mLeft = copyMatrix(parMatrix)
+    # melakukan pergeseran ke kiri
+    moveLeft(mLeft)
+    # membuat node baru sesuai atribut pada kelas node
+    nodeLeft = pq.node(parMatrix,mLeft, cost(mLeft), parent.level+1, "left")
+    # jika node yang baru dibuat tidak ada di list of visitedNodes, maka node akan ditambahkan ke list of nodes
+    if isNotInListOfNodes(nodeLeft, visitedNodes):
+        generatedNodes.append(nodeLeft)
+
+    # mereturn nodes yang telah dibangkitkan 
+    return generatedNodes
+
+def BnB(m):
+    # Penjelasan:
+    # Fungsi ini dilakukan untuk menyelesaikan persoalan 15 puzzle dengan algoritma branch and bound
+    # Fungsi akan berhenti saat dalam keadaan goal state
+
+    # menginisialisasikan priority queue
+    PQ = pq.priorityQueue()
+
+    # membuat node awal yaitu node dengan matrix yang sama dengan matrix awal
+    parent = pq.node(None,m,cost(m), 0, "initial position")
+
+    # menginisialisasi array of visited nodes
+    visitedNodes = []
+
+    # menginisialisasi array of generated nodes
+    generatedNodes = [parent]
+
+    # menambahkan node awal ke priority queue
+    PQ.push(parent)
+
+    step = 0
+    while not(PQ.isEmpty()):
+        # pop elemen paling depan prioqueue
+        parent = PQ.pop()     
+
+        # menambahkan node yang sekarang akan dibangkitkan sebagai visited node
+        visitedNodes.append(parent)
+
+        # mendisplay info untuk iterasi sekarang
+        displayInfo(step, parent)
+
+        # jika cost = 0 maka sudah mencapai goal state
+        if parent.cost == 0:
+            print("\n\( ﾟヮﾟ)/ -- YOUR PUZZLE IS SOLVED! -- ٩(◍╹∀╹◍)۶")
+            print("\nTotal Nodes Generated:", len(generatedNodes),"nodes")
+            return parent
+        else:
+            # membangkitkan node child sesuai dengan parent
+            childNodes = generateChildNode(parent, visitedNodes)
+            # menambahkan node child ke prioqueue dengan priority berupa cost(depth+tile tidak kosong yang berbeda dengan solusi)
+            for child in childNodes:
+                PQ.push(child)
+                generatedNodes.append(child)
+
+        # step akan bertambah satu tiap penelusuran pohon statusn
+        step += 1
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import time
 import functions
 
+# fungsi untuk input file berupa txt
 def inputFile():
     file = input("Enter file to read: ")
     with open ('../test/'+file, 'r') as f:
@@ -9,11 +10,15 @@ def inputFile():
             m.append( [ int (x) for x in line.split(' ') ] )
         return m
 
+# fungsi untuk input berupa manual 
 def inputKeyboard():
     m = [[0 for _ in range(4)] for _ in range(4)]
     for i in range (4):
         for j in range (4):
             m[i][j] = int(input("Enter value for position ["+str(i)+","+str(j)+"]: "))
+            while (m[i][j]> 16 or m[i][j] == 0):
+                print("Please only input number 1-15 or 16 for blank space!")
+                m[i][j] = int(input("Enter value for position ["+str(i)+","+str(j)+"]: "))
     return m
 
 print("""
@@ -32,38 +37,48 @@ print("""
 """)
 
 x = int(input("Main Menu \n1. Input file.txt \n2. Input Keyboard\nYour Choice: "))
-if x==1 or x==2:
-    if x == 1:
-        m = inputFile()
-    elif x == 2:
-        m = inputKeyboard()
-    if(not functions.reachableGoal(m)):
-            print("\nYour Puzzle can't be Solved")
-            print("""
+
+while (not(x==1 or x==2)):
+    print("Invalid Choice! Please Choose either 1 or 2")
+    x = int(input("Main Menu \n1. Input file.txt \n2. Input Keyboard\nYour Choice: "))
+
+if x == 1:
+    m = inputFile()
+elif x == 2:
+    m = inputKeyboard()
+    
+# mengecek apakah goal reachable dengan metode heuristik
+if(not functions.reachableGoal(m)):
+        print("INITIAL PUZZLE STATE")
+        functions.displayMatrix(m)
+        print("\nYour Puzzle can't be Solved")
+        print("""
 ──▄────▄▄▄▄▄▄▄────▄───
 ─▀▀▄─▄█████████▄─▄▀▀──
 ─────██─▀███▀─██──────
 ───▄─▀████▀████▀─▄────
 ─▀█────██▀█▀██────█▀──
-            """)
-    else:
-            print("\nYour Puzzle can be Solved! :) \n")
-            print("""
+        """)
+else:
+        print("\nYour Puzzle can be Solved! :) \n")
+        print("""
 ───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
 ───█▒▒░░░░░░░░░▒▒█───
 ────█░░█░░░░░█░░█────
 ─▄▄──█░░░▀█▀░░░█──▄▄─
 █░░█─▀▄░░░░░░░▄▀─█░░█
-            """)
-            
-            rootCost = functions.cost(m)
-            if rootCost==0:
-                print("PUZZLE ALREADY SOLVED!")
-            else:
-                start_time = time.time()
-                functions.BnB(m)
-                print("Program executed in %s seconds" % round((time.time() - start_time),5))
-else:
-    print("Invalid Choice!")
-    exit()
+        """)
+        rootCost = functions.cost(m)
+        # apabila initial state puzzle sudah berupa susunan solusi maka tidak perlu menjalankan pencarian solusi
+        if rootCost==0:
+            print("INITIAL PUZZLE STATE")
+
+            functions.displayInfon(m)
+            print("PUZZLE ALREADY SOLVED!")
+        else:
+            start_time = time.time()
+            # pemanfaatan algoritma branch and bound untuk pencarian solusi
+            functions.BnB(m)
+            print("Program executed in %s seconds" % round((time.time() - start_time),5))
+
 
